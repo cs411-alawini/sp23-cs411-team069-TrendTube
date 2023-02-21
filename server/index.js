ff99d94b4e4826e3d5160136d57c21e2a10707da
lohitm2 <lohit308@gmail.com>
2023-02-20 20:50:45 -0600

    Started CRUD functionality

--- server/index.js
+++ server/index.js
@@ -12,13 +12,34 @@ app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-
-// req -> getting info from frontend
-// res -> sending info to front end
+// GET Request: Fetch Data
+// @req -> getting info from frontend
+// @res -> sending info to front end
 app.get('/', (req,res) => {
     res.send('Hello World!?!')
 });
 
-app.listen(3001, () => {
-    console.log('Port 3001 Running')
+// POST Request: Create Data
+// @req -> getting info from frontend
+// @res -> sending info to front end
+app.post('/', (req,res) => {
+    res.send('Hello World!?!')
+});
+
+// PUT Request: Update Data else Create Data
+// @req -> getting info from frontend
+// @res -> sending info to front end
+app.put('/', (req,res) => {
+    res.send('Hello World!?!')
+});
+
+// DELETE Request: Delete Data
+// @req -> getting info from frontend
+// @res -> sending info to front end
+app.delete('/', (req,res) => {
+    res.send('Hello World!?!')
+});
+
+app.listen(4000, () => {
+    console.log('Port 4000 Running')
 });
