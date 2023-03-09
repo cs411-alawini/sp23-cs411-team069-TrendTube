ce0d6a9fb08337e9c1a94e35166dfdabf3b6e67d
Lorenzo Bujalil Silva <lorenzo@wirelessprv-10-193-58-158.near.illinois.edu>
2023-03-09 14:31:26 -0600

    changed script for names

--- database/createNames.py
+++ database/createNames.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 df = pd.read_csv('names.csv')
-users_table = pd.DataFrame(columns=['userId, userName, userPassword, userEmail'])
+users_table = pd.DataFrame(columns=[])
 for idx, row in df.iterrows():
     id = row['Id']
     name = row['Name']
@@ -10,7 +10,5 @@ for idx, row in df.iterrows():
 
     user = {'userId':str(id), 'userName': name, 'userPassword':password, 'userEmail': email }
     users_table = pd.concat([users_table, pd.DataFrame(user, index=[0])], ignore_index=True)
-    
 
-users_table = users_table[['userId', 'userName', 'userPassword', 'userEmail']]
 users_table.to_csv('users.csv')
