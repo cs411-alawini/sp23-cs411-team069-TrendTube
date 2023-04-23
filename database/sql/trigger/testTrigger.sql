6a5c93b1d1dd3cb0d106e83f18dc1a82426c75eb
lohitm2 <lohit308@gmail.com>
2023-04-22 19:23:42 -0500

    Implemented trigger with advanced query

--- database/sql/trigger/testTrigger.sql
+++
@@ -1,6 +0,0 @@
-CREATE DEFINER=`root`@`localhost` TRIGGER `watchedvideos_AFTER_INSERT` AFTER INSERT ON `watchedvideos` FOR EACH ROW BEGIN
-    /*SIGNAL SQLSTATE '02000' SET MESSAGE_TEXT=NEW.WatchedDate;*/
-    IF  NEW.WatchedDate IS NULL THEN
-		INSERT INTO recommendedvideos VALUES (NEW.video_id, NEW.user_Id, NEW.video_id);
-    END IF;
-END
